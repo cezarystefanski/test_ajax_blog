0aa8b44b9a186c7ac85c5681e67964251c8f5416
Cezary Bogusław Stefański <cezary.stefanski@maxymiser.com>
2016-03-04 19:38:37 +0100

    fixes. now loading 3 at once

--- app.js
+++ app.js
@@ -2,16 +2,21 @@
  * Created by Cezary on 2016-03-04.
  */
 
-;(function() {
+;(function () {
     "use strict";
 
     var $ = window.jQuery;
 
     var counter = 0;
     var $noMore = $("<p></p>");
+    var endFlag;
     $noMore.text("No more stories.");
 
-    $(".loader").on("click", function () {
+    $(document).ready(loadArticles);
+
+    $(".loader").on("click", loadArticles);
+
+    function loadArticles() {
         $(".loading").css("display", "block");
         var template;
         var responseObj;
@@ -27,28 +32,42 @@
         $.when(dataPromise, templatePromise)
             .then(function (data, temp) {
                 data = data.reverse();
-                template = temp;
-                if (data[counter]) {
-                    responseObj = data[counter];
+
+                if (!data[counter]) {
+                    endFlag = true;
+                } else if (data[counter + 3]) {
+                    responseObj = data.slice(counter, counter + 3);
                 } else {
+                    responseObj = data.slice(counter);
+                    endFlag = true;
+                }
+
+                template = temp;
+
+                if (responseObj) {
+                    responseObj.forEach(function (post) {
+                        var $template = $(template);
+                        $template.attr("id", "article-" + counter);
+                        $template.find(".title").html(post.title);
+                        $template.find(".date").html(post.date);
+                        $template.find(".text").html(post.text);
+
+                        $(".textBox").append($template);
+                        $(".loading").css("display", "none");
+                        counter++;
+                    });
+                }
+
+                $('html, body').animate({
+                    scrollTop: $("section#article-" + (counter - 1)).offset().top
+                }, 50);
+
+                if (endFlag) {
                     $(".loading").css("display", "none");
                     $(".textBox").append($noMore);
                     $(".loader").css("display", "none");
                     return;
                 }
-                var $template = $(template);
-
-                $template.attr("id", "article-" + counter);
-                $template.find(".title").append(responseObj.title);
-                $template.find(".date").append(responseObj.date);
-                $template.find(".text").append(responseObj.text);
-
-                $(".textBox").append($template);
-                $(".loading").css("display", "none");
-                $('html, body').animate({
-                    scrollTop: $("section#article-" + counter).offset().top
-                }, 50);
-                counter++;
             });
-    })
+    }
 })();
